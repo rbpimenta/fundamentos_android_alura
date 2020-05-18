2b6a9b2be80623489ac0063bda0248222f28e684
Rodrigo Pimenta <rodrigopimenta2@gmail.com>
2020-05-18 05:54:25 -0300

    Adicionar nova atividade do Formulário de Aluno Refatoramento do código

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,4 +1,4 @@
-package br.com.alura.agenda;
+package br.com.alura.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,21 +10,23 @@ import android.widget.Toast;
 import java.util.Arrays;
 import java.util.List;
 
+import br.com.alura.agenda.R;
+
 // AppCompatActivity -> ele é uma boa prática no Android, por dar suporte a versões anteriores do Android
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Iniciando APP", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_lista_alunos);
 
         // Adicionar título
         setTitle("Lista de Alunos");
 
-        Toast.makeText(this, "Iniciando APP", Toast.LENGTH_LONG).show();
-        setContentView(R.layout.activity_main);
-
+        // Adicionando lista de alunos
         List<String> alunos = Arrays.asList("Alex", "Fran", "Jose", "Rodrigo");
-        ListView listViewAlunos = findViewById(R.id.activity_main_lista_alunos);
+        ListView listViewAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listViewAlunos.setAdapter(
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos)
         );
