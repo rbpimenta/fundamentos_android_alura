269cc97ea94d279c0a36b91dc3c5a3b348fa0ce9
Rodrigo Pimenta <rodrigopimenta2@gmail.com>
2020-05-18 06:12:56 -0300

    Adicionar listener para Salvar aluno

--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,6 +2,9 @@ package br.com.alura.agenda.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import br.com.alura.agenda.R;
 
@@ -11,5 +14,15 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        Button botaoSalvarAluno = findViewById(R.id.activity_formulario_aluno_salvar);
+        botaoSalvarAluno.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(FormularioAlunoActivity.this, "Salvar Aluno", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
